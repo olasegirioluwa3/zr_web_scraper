83ffda4c02da0e3f0ae1f0520ed54cb16bf462ff
olasegirioluwa3 <olasegirioluwafemi@gmail.com>
2024-10-26 18:51:26 +0100

    added amazon scraping module

---
+++ tests/test_zr_amazon_scraper.py
@@ -0,0 +1,52 @@
+import unittest
+from unittest.mock import patch
+
+import requests
+# from zr_amazon_scraper import AmazonScraper
+from zr_amazon_scraper.scraper import AmazonScraper
+# from zr_web_scraper.zr_amazon_scraper import AmazonScraper
+
+
+class TestAmazonScraper(unittest.TestCase):
+
+    def setUp(self):
+        self.scraper = AmazonScraper()
+
+    # @patch('scraper.amazon_scraper.scraper.requests.get')
+    @patch('zr_amazon_scraper.scraper.requests.get')
+    def test_scrape_no_results(self, mock_get):
+        # Simulate a response with no results
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.text = '<div class="g"></div>'  # Empty search results
+        
+        result = self.scraper.scrape("No Results Query")
+        self.assertIn("No search results found for query", result)
+
+    # @patch('scraper.amazon_scraper.scraper.requests.get') 
+    @patch('zr_amazon_scraper.scraper.requests.get')
+    def test_scrape_success(self, mock_get):
+        # Simulate a successful response with results
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.text = '''
+            <div class="g">
+                <h3 class="LC20lb">Test Title</h3>
+                <a href="http://example.com">Link</a>
+                <div class="VwiC3b">Test Content</div>
+            </div>
+        '''
+        
+        result = self.scraper.scrape("Test Query")
+        self.assertEqual(len(result), 1)  # One result should be found
+        self.assertEqual(result[0]['title'], 'Test Title')
+
+    # @patch('scraper.amazon_scraper.scraper.requests.get')
+    @patch('zr_amazon_scraper.scraper.requests.get')
+    def test_scrape_failure(self, mock_get):
+        # Simulate a network error
+        mock_get.side_effect = requests.exceptions.RequestException("Network Error")
+        
+        result = self.scraper.scrape("Error Query")
+        self.assertIn("Failed to retrieve search results:", result)
+
+if __name__ == '__main__':
+    unittest.main()
